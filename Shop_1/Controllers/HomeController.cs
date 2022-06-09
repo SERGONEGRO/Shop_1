79888262381705f8df923eb62791b8a7d60a2ae6
Сергей Воробьев <kazan1003@gmail.com>
2022-06-09 16:55:10 +0300

    in work

---
+++ Shop_1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_1.Data.Interfaces;
+using Shop_1.ViewModels;
+
+namespace Shop_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// список продуктов
+        /// </summary>
+        private IAllProducts _productRep;
+
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="productRep"></param>
+        /// <param name="shopCart"></param>
+        public HomeController(IAllProducts productRep)
+        {
+            _productRep = productRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeProducts = new HomeViewModel
+            {
+                FavProducts = _productRep.GetFavProduct
+            };
+            return View(homeProducts);
+        }
+    }
+}
---
+++ Shop_1/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Shop_1.Data.Models;
+
+namespace Shop_1.ViewModels
+{
+    /// <summary>
+    /// на основе этой модели создаем объекты и записываем в него нужные значения,
+    /// чтобы потом передать во View 1 обьект, а не несколько
+    /// </summary>
+    public class HomeViewModel
+    {
+        /// <summary>
+        /// избранные продукты
+        /// </summary>
+        public IEnumerable<Product> FavProducts { get;set; }
+
+    }
+}
